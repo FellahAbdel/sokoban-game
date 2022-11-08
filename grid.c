50590d25ea72025f88df327d5385cd128c555ef9
abdoul aziz diallo <abdoul-aziz.diallo@etu.unistra.fr>
2022-11-08 23:05:15 +0100

    Joueur repéré

--- grid.c
+++ grid.c
@@ -53,6 +53,13 @@ grid initLevel(const char* filePath){
 		while(*buffer && *buffer != '\n'){
             /// On remplit en même temps, la grille du jeux
             gridInit->game_grid[current_row][current_column] = *buffer;
+
+            // On recupère les coordonnées du joueur
+            if(*buffer == '@'){
+                gridInit->aPlayer.x = current_row;
+                gridInit->aPlayer.y = current_column;
+            }
+
 			current_column += 1;
 			buffer += 1;
 		}		
