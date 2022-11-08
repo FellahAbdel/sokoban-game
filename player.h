28451622c787dabba825334ff620c4b803c05535
abdoul aziz diallo <abdoul-aziz.diallo@etu.unistra.fr>
2022-11-08 23:08:40 +0100

    Création de enum Direction

--- player.h
+++ player.h
@@ -11,11 +11,21 @@
  * @brief Cette structure contient les informations 
  * concernant liées à la position du joueur
  */
-
 typedef struct Player{
     int x;
     int y;
 }player;
+
+/**
+* @enum Direction player.h
+* Définit le sens de direction d'une case 
+*/
+enum Direction {
+    Top, 
+    Left,
+    Right,
+    Bottom
+}
 
 
 #endif
